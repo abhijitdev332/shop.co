b203bf280e1a147c1b852bfd369bfdcb4c4ffe66
abhijit ghanta <abhijit.ghanta@klizos.com>
2025-01-17 18:51:50 +0530

    hotfix

--- src/utils/ProtectedRoute.ts
+++ src/utils/ProtectedRoute.ts
@@ -3,12 +3,12 @@ import { Navigate } from "react-router-dom";
 
 const GuestProtected = ({ children }) => {
   const [user, status] = useUser();
-  status ? children : <Navigate to={"/"} />;
+  return status ? children : <Navigate to={"/"} />;
 };
 
 const UserProtected = ({ children }) => {
   const [user, status] = useUser();
-  status ? <Navigate to={"/"} /> : children;
+  return status ? <Navigate to={"/"} /> : children;
 };
 
 export { GuestProtected, UserProtected };
